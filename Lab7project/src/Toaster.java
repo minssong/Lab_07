8538e3f92101402983f195c9e4aa91e550dad871
minsong Kim <minsong20@naver.com>
2017-04-27 11:34:25 +0900

    a

---
+++ Lab7project/src/Toaster.java
@@ -0,0 +1,14 @@
+
+public class Toaster extends Product{
+
+	public void Toaster(){
+		
+	}
+	public String prepareFood(){
+		
+		return "Food";
+	}
+	public String getName(){
+		return "Toaster";
+	}
+	}
