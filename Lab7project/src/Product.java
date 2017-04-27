537c98c1c5414844f7d93de4a6e08ea8466a677a
minsong Kim <minsong20@naver.com>
2017-04-27 11:30:28 +0900

    fgh

---
+++ Lab7project/src/Product.java
@@ -0,0 +1,21 @@
+
+public abstract class Product {
+
+	double price;
+	
+	public void Product(){
+		
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public String getName(){
+		return "Lab_07!";
+	}
+}
